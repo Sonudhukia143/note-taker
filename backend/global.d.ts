1af30c0cf7e0e8f2c93fadaa899651b81d9d74d0
Sonudhukia143 <jagdishdhukia770@gmail.com>
2025-09-29 21:44:17 +0530

    Dashboard : Note data and Dashboard add with live sync service

---
+++ backend/global.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="node" />
+/// <reference types="express" />
+/// <reference types="mongoose" />
+/// <reference types="cors" />
+/// <reference types="dotenv" />
+/// <reference types="nodemailer" />
+/// <reference types="jsonwebtoken" />
